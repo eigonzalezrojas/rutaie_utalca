fc66332815fa28cd42677b3d63d96f8bc1f8af60
eigonzalezrojas <eithelgonzalezrojas@gmail.com>
2023-09-05 23:43:24 -0300

    insert carrusel

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,3 +49,85 @@ window.addEventListener("scroll", function () {
   window.scrollY >= 800 ? goTopBtn.classList.add("active") : goTopBtn.classList.remove("active");
 
 });
+
+
+window.addEventListener('load', function(){
+  var currentItem = null;
+  var acordion = document.querySelector('.acord_container');
+ 
+  acordion.onmouseenter = function(e){
+   var items = document.querySelectorAll('.item_wrap');
+    for(var i = 0; i<items.length; i++){
+      if(!items[i].classList.contains('col_50')){
+        items[i].classList.remove('col_30');
+        items[i].classList.add('col_25');
+      }
+    }
+  };
+  
+  acordion.onmouseover = function(e){
+    if(currentItem){
+      return;
+    }
+    var e = e || window.event;
+    var target = e.target;  
+    while(target != this){
+       if(target.classList.contains('item_wrap')) break;
+       target = target.parentNode;
+    }
+    if(target == this) return;
+    
+    currentItem = target;
+    //action
+  if(target.classList.contains('col_25')){
+      target.classList.remove('col_25');
+      target.classList.add('col_50');
+      }else if(target.classList.contains('col_30')){
+        target.classList.remove('col_30');
+        target.classList.add('col_50');
+      }else{
+        target.classList.add('col_50');
+      }
+    target.querySelector('.item_text').classList.add('active');           
+    target.querySelector('.item_btn').classList.add('active');     
+    target.querySelector('.h1').classList.add('active');
+  }
+  
+  acordion.onmouseout = function(e){
+    var e = e || window.event;
+    var target = e.target;
+    if(!currentItem){
+      return;
+    }
+    var relatedTarget =  e.relatedTarget;
+    if(relatedTarget){
+      while(relatedTarget){
+        if(relatedTarget == currentItem) return;
+        relatedTarget = relatedTarget.parentNode;
+      }
+    }
+    while(target != this){
+       if(target.classList.contains('item_wrap')) break;
+       target = target.parentNode;
+    }
+    //action
+    if(target.classList.contains('col_50')){
+      target.classList.remove('col_50');
+      target.classList.add('col_25');
+    }
+     target.querySelector('.item_text').classList.remove('active');
+     target.querySelector('.item_btn').classList.remove('active');
+    target.querySelector('.h1').classList.remove('active');
+    currentItem = null;
+  };
+  
+  acordion.onmouseleave = function(){
+    var items = document.querySelectorAll('.item_wrap');
+    for(var i = 0; i<items.length; i++){
+      if(items[i].classList.contains('col_25')){
+        items[i].classList.remove('col_25');
+        items[i].classList.add('col_30');
+      }
+    }
+  }
+});
